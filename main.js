da1c49e76e474da2ea6afbcaf5b1c63b9571c9a0
NikitaNiko1enko <130448990+NikitaNiko1enko@users.noreply.github.com>
2024-04-02 20:41:49 +0200

    upDate

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ if(navClose){
     })
 }
 
-
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
@@ -76,6 +75,37 @@ const sr = ScrollReveal({
     delay: 300,
     // reset: true, // Animations repeat
 })
+
+
+document.addEventListener('DOMContentLoaded', () => {
+   
+    var swiper = new Swiper(".slide-content", {
+         slidesPerView: 2,
+        spaceBetween: 25,
+        loop: true,
+        centerSlide: 'true',
+        fade: 'true',
+        gragCursor: 'true',
+        loopFillGroupWithBlank: 'true',
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+
+        breakpoints:{
+            0:{
+                slidesPerView: 1,
+            },
+            520:{
+                slidesPerView: 2,
+            },
+        },
+    });
+    });
 
 sr.reveal(`.home__img , .new__data , .care__img , .contact__content , .footer`)
 sr.reveal(`.home__data , .care__list ,.contact__img`  , {delay: 500})  
